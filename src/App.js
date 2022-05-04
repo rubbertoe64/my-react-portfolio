d4cf19aa4c67fda4fd645c57afabdf3dd91d0d98
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-04 15:45:42 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import "./styles.css"
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Navbar/>
+    <Footer />
+  </div>
+  );
+}
+
+export default App;
