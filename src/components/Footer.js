3d12765833d92c69ede89fa1394550497c734035
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-04 15:46:12 -0700

    footer code

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles.css';
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <div className='footer-flex'>
+                <h3 className='footer-text'>Email: <a href='#'>rgonzelz7359@gmail.com</a></h3>
+                <h3> Number:(760)897-6104</h3>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Footer;
