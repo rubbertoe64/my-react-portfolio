a9168ca30d20100700300ca09333839eb53dade1
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-05 23:47:22 -0700

    added code  navigate the pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 import '../styles.css'
 
 
-function Navbar({currentPage, handlePageChange}){
+function Navbar(){
      return(
-          <div>alo</div>
+          <div className='topnav'>
+          <section className='name'>
+              <a href='/About'>Roberto Gonzalez</a>
+          </section>
+          <section className='topnav-right'>
+              <Link to='/About'>About me</Link>
+              <Link to='/Portfolio'>Portfolio</Link>
+              <Link to='/Contact'>Contact</Link>
+              <Link to='/Resume'>Resume</Link>
+          </section>
+          
+      </div>
      )
 };
 
