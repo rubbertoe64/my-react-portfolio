e5c937a0c41372c10c817e6d79337f76b23fa8ec
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-05 23:47:00 -0700

    footer and header js added for loading nav bar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styles.css';
+import githubIcon from "./images/GitHub-Mark-64px.png"
 
 function Footer(){
     return(
         <div className='footer'>
             <div className='footer-flex'>
-                <h3 className='footer-text'>Email: <a href='#'>rgonzelz7359@gmail.com</a></h3>
+                <h3 className='footer-text'>Email: <a href="mailto:rgonzelz7359@gmail.com">rgonzelz7359@gmail.com</a></h3>
                 <h3> Number:(760)897-6104</h3>
+                <img src={githubIcon} alt="github icon" className="footer-github" onClick={sendGithub}></img>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png' alt='linkdin logo' className='footer-linkdin'></img>
             </div>
         </div>
     )
     
 }
+function sendGithub(){window.open("https://github.com/rubbertoe64", "_blank")}
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,7 @@ import '../styles.css'
 
 function Header(){
     return (
-        <div className='topnav'>
-            <section className='name'>
-                <a href='#'>Roberto Gonzalez</a>
-            </section>
-            <section className='topnav-right'>
-                <a href='/About'>About me</a>
-                <a href='/Portfolio'>Portfolio</a>
-                <a href='/Contact'>Contact</a>
-                <a href='/Resume'>Resume</a>
-            </section>
-            
-        </div>
-        
-       
-        
+       <Navbar/>
     )
 }
 
