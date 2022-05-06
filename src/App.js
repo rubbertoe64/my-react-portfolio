87cf9ee3a4d071fb45d80a8c2624da19f09e117d
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-05 23:43:41 -0700

    used app routing to render main components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import './App.css';
 import Header from "./components/Header"
-import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { About, Portfolio, Contact, Resume } from "./components/pages"
 import "./styles.css"
+
+
+
 
 function App() {
   return (
     <div>
-    <Header />
-    <Navbar/>
-    <Footer />
-  </div>
+    <Router>
+      
+       <Header />
+       <div className="main-body">
+         <Switch >
+          <Route exact path="/" render={() => {
+            return(
+              <Redirect to="/About" />
+            )
+          }} />
+          <Route exact path="/About" component={About} />
+            {/* <About/> */}
+          {/* </Route> */}
+          <Route exact path="/Portfolio" component={Portfolio}/>
+            {/* <h1>portfolio</h1> */}
+          {/* </Route> */}
+          <Route exact path="/Contact" component={Contact} />
+          
+          
+          <Route exact path="/Resume" component={Resume}/>
+        </Switch>
+       </div>
+        
+       <Footer />
+      
+    </Router>
+    </div>
   );
 }
 
