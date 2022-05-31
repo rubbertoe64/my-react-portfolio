d399f1bd04b8b641a1915d56baa53ca1cb7eda9c
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-30 23:46:18 -0700

    redid whole card system to dynamically render info

--- src/components/pages/Cards.js
+++ src/components/pages/Cards.js
@@ -1,51 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./cards.css"
 import githubIcon from "../images/GitHub-Mark-64px.png"
 import githubIconLight from "../images/GitHub-Mark-Light-64px.png"
+import { Button, Text, Heading, Center, Box, useColorModeValue, Image, Stack, Avatar, Grid, SimpleGrid } from "@chakra-ui/react";
+import axios from "axios";
+
 
 
 
 function Cards() {
-    return (
-        <div className="card-container w3-animate-top">
-            <div className="css-card" id="first-project">
-                <h3 className="card-header">Note Taker</h3>
-                <p>This is my note-taker app! It should be able to create new notes as wel as view older notes or delete them if you want.</p>
-                <img src={githubIcon} alt="Github Icon" className="github-icon" id="githubIcon1" onClick={githubIcon1}/>
-                <img src="https://icon-library.com/images/world-icon-png/world-icon-png-4.jpg" alt="World Icon" className="world-icon" id="worldIcon1" onClick={worldIcon1}/>
-            </div>
-            <div className="css-card" id="second-project">
-                <h3 className="card-header">Restroom Finder</h3>
-                <p>Website to help users find a nearby restroom!</p>
-                <img src={githubIcon} alt="Github Icon" className="github-icon" id="githubIcon2" onClick={githubIcon2}/>
-                <img src="https://icon-library.com/images/world-icon-png/world-icon-png-4.jpg" alt="World Icon" className="world-icon" id="worldIcon2" onClick={worldIcon2}/>
+    const [repos, setRepos] = useState([])
+    const getData = () => {
+        axios('https://api.github.com/users/rubbertoe64/starred').then(response => {
+            setRepos(response.data)
+        })
+        
+    }
+    useEffect(() => getData(), [])
+    console.log(repos)
 
-            </div>
-            <div className="css-card" id="third-project">
-                <h3 className="card-header">Weather Page</h3>
-                <p>Look at your city's weather!</p>
-                <img src={githubIcon} alt="Github Icon" className="github-icon" id="githubIcon3" onClick={githubIcon3}/>
-                <img src="https://icon-library.com/images/world-icon-png/world-icon-png-4.jpg" alt="World Icon" className="world-icon" id="worldIcon3" onClick={worldIcon3}/>
-            </div>
-            <div className="css-card" id="fourth-project">
-                <h3 className="card-header">Work Day Scheduler</h3>
-                <p>This is my work day calendar! Keep track of your own events by typing in the text box and saving it! The colors display if the event is in the past with grey, present with red, and future with green.</p>
-                <img src={githubIcon} alt="Github Icon" className="github-icon" id="githubIcon4" onClick={githubIcon4}/>
-                <img src="https://icon-library.com/images/world-icon-png/world-icon-png-4.jpg" alt="World Icon" className="world-icon" id="worldIcon4" onClick={worldIcon4}/>
-            </div>
-            <div className="css-card" id="fifth-project">
-                <h3 className="card-header">Employee Tracker</h3>
-                <p>Track your Employees with this Nodejs app!</p>
-                <img src={githubIconLight} alt="Github Icon" className="github-icon" id="githubIcon5" onClick={githubIcon5}/>
+    
+    
+    return (
+        <SimpleGrid columns={3} spacing={5} minChildWidth='420px' className="w3 w3-animate-top">
+        {repos.map((repo) => {
+            return(
+            <Center py={6} key={repo.id}>
+        
+            <Box
+            maxW={'445px'}
+            w={'full'}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            p={6}
+            bg={'black'}
+            overflow={'hidden'}>
+                <Box>
+                    <Center>
+                        <Heading>{repo.description}</Heading>
+                    </Center>
+                    
+                </Box>
+            
+            <Stack>
+                <Center>
+                    <h3
+                    fontSize={'2xl'}
+                    fontFamily={'body'}>
+                    {`Repo Create Date: ${new Date(repo.created_at).toLocaleDateString('en-US')}`}
+                    </h3>
+                </Center>
                 
-            </div>
-            <div className="css-card" id="sixth-project">
-                <h3 className="card-header">Ecommerce-Backend</h3>
-                <p>Connect to a database using Sequelize and use commands!</p>
-                <img src={githubIconLight} alt="Github Icon" className="github-icon" id="githubIcon6" onClick={githubIcon6}/>
-                
-            </div>
-        </div>
+                <Text color={'gray.500'}>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum.
+                </Text>
+            </Stack>
+            
+            </Box>
+        </Center>
+            )
+        })}
+           
+        </SimpleGrid>
+
+        
+     
     )
     
 };
@@ -58,35 +80,7 @@ function githubIcon1 () {window.open("https://github.com/rubbertoe64/note-taker"
 
 function worldIcon1 () {window.open("https://note-taker-api-online.herokuapp.com/", "_blank") ;}
 
-// 2
-function githubIcon2 () {window.open("https://github.com/Will-Pikus/Restroom-Finder", "_blank") ;}
-    
 
-
-function worldIcon2 () {window.open("https://will-pikus.github.io/Restroom-Finder/", "_blank") ;}
-
-
-function githubIcon3 () {window.open("https://github.com/rubbertoe64/weather-page", "_blank") ;}
-    
-
-
-function worldIcon3 () {window.open("https://rubbertoe64.github.io/weather-page/", "_blank") ;}
-
-
-function githubIcon4 () {window.open("https://github.com/rubbertoe64/work-calendar", "_blank") ;}
-    
-
-
-function worldIcon4 () {window.open("https://rubbertoe64.github.io/work-calendar/", "_blank") ;}
-
-
-function githubIcon5 () {window.open("https://github.com/rubbertoe64/employee-tracker", "_blank") ;}
-    
-
-
-
-function githubIcon6 () {window.open("https://github.com/rubbertoe64/e-commerce-backend", "_blank") ;}
-    
 
 
 export default Cards;
