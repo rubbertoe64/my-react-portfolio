a682fb27e64f60287f82d27f367f4b51340c59f9
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-05-31 00:09:32 -0700

    made link work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,13 @@ function Footer(){
                 <h3 className='footer-text'>Email: <a href="mailto:rgonzelz7359@gmail.com">rgonzelz7359@gmail.com</a></h3>
                 <h3> Number:(760)897-6104</h3>
                 <img src={githubIcon} alt="github icon" className="footer-github" onClick={sendGithub}></img>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png' alt='linkdin logo' className='footer-linkdin'></img>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png' onClick={sendLinkedIn}  alt='linkdin logo' className='footer-linkdin'></img>
             </div>
         </div>
     )
     
 }
 function sendGithub(){window.open("https://github.com/rubbertoe64", "_blank")}
+function sendLinkedIn(){window.open("https://www.linkedin.com/in/roberto-gonzalez-91a652222/", "_blank")}
 
 export default Footer;
